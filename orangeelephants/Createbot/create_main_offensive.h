0e2ed49a8cdc43643529c816a1f0a364b7ee321b
rsamvit <rsamvit@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2010-07-08 18:58:26 +0000

    added files for offensive tactics

---
+++ orangeelephants/Createbot/create_main_offensive.h
@@ -0,0 +1,11 @@
+#ifndef _CREATE_MAIN_OFFENSIVE_H
+#define _CREATE_MAIN_OFFENSIVE_H
+
+void create_main_offensive();
+void turn(float, int);
+void move_to_distance(int, int);
+float inchToMm(int);
+void move_to_frogs();
+void test();
+
+#endif
