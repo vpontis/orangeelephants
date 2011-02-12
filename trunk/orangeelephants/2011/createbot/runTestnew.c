30359c11ba83f99e0b7b7512c9d9c8e837e0f48a
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-02-12 23:28:04 +0000

    Changed constants for the create from camelCase to CONSTANTS_WITH_UNDERSCORES.

--- trunk/orangeelephants/2011/createbot/runTestnew.c
+++ trunk/orangeelephants/2011/createbot/runTestnew.c
@@ -4,7 +4,7 @@
 int main()
 {
 	enable_servos();
-	set_servo_position(clawPort, clawOpenPos);
+	set_servo_position(CLAW_PORT, CLAW_OPEN_POS);
 	printf("Press A button to run program \n");
 	printf("Press UP to raise arm. Press DOWN to callibrate and to lower arm \n");
 	while(1)
@@ -22,7 +22,7 @@ int main()
 		else if(down_button())
 		{
 			moveArmUp();
-			moveArmDown(armPosDown);
+			moveArmDown(ARM_POS_DOWN);
 		}        
 	}
 	
