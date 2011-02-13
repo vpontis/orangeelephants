8e61708e682aa51a4d00215c05de132e4cb87d67
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-02-13 00:49:06 +0000

    Changed format and renamed several create functions.

--- orangeelephants/2011/createbot/createConstants.h
+++ orangeelephants/2011/createbot/createConstants.h
@@ -3,12 +3,20 @@
 
 //moveToDistance(int distance, int speed)
 const double  STOP_DISTANCE_RATIO = 0.4; //needs callibration
+
 const int  ARM_DOWN_VELOCITY = -400; //needs callibration
 const int  ARM_UP_VELOCITY = 750; //needs callibration
-const int  ARM_MOTOR_PORT = 0; 
-const int  CLAW_PORT = 2; 
+
+
 const int  CLAW_OPEN_POS = 790; 
 const int  ARM_POS_DOWN = -8500;
 const int  ARM_PARTIAL_DOWN  = -600;
+
+//motor & Ssensor ports
+const int  ARM_MOTOR_PORT = 0; 
+const int  CLAW_PORT = 2; 
+const int  ARM_TOUCH_PORT = 15;
+
+
 
 #endif
