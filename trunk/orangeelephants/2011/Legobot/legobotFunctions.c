054a86a2a979d294e688c631787fb234beed4239
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-05 23:18:42 +0000

    Changed move to distance. Motors still DO NOT work.

--- trunk/orangeelephants/2011/Legobot/legobotFunctions.c
+++ trunk/orangeelephants/2011/Legobot/legobotFunctions.c
@@ -9,7 +9,7 @@ void legobotAccel(int initSpeed,int finalSpeed){
         int increm = (finalSpeed-initSpeed)/20;
         int speed = initSpeed;
         
-        float time = abs((finalSpeed-initSpeed)/2500);	//delay is .1 sec for accel from 0-500
+        float time = abs((finalSpeed-initSpeed)/2500);  //delay is .1 sec for accel from 0-500
         
         if(speed<finalSpeed)		//if you are accelerating
         {
@@ -21,7 +21,7 @@ void legobotAccel(int initSpeed,int finalSpeed){
      
 				}
 		}		
-        else						//if you are decelerating
+        else				//if you are decelerating
         {
                 while(speed>finalSpeed)
                 {
@@ -36,90 +36,107 @@ void legobotAccel(int initSpeed,int finalSpeed){
         
 }
 
-void moveToDistance(float distance, int speed) {	//moves for a certain distance
+
+void moveToDistance(float distance, int speed) {
 	clear_motor_position_counter(L_MOTOR); 
 	clear_motor_position_counter(R_MOTOR); 
-    mrp(L_MOTOR, speed, cmToTicks(distance));		//left wheel
-    mrp(R_MOTOR, speed, cmToTicks(distance));		//right wheel
-    bmdMotors();
+	if(distance>0)	{
+		while(get_motor_position_counter(R_MOTOR) < cmToTicks(distance))	{
+			mav(L_MOTOR, speed); 
+			mav(R_MOTOR, speed);
+			//mrp(R_MOTOR, speed, cmToTicks(distance));    
+		}
+	}
+	else	{
+		while(get_motor_position_counter(R_MOTOR) > cmToTicks(distance))	{
+			mav(L_MOTOR, -speed); 
+			mav(R_MOTOR, -speed);
+			//mrp(R_MOTOR, speed, cmToTicks(distance));    
+		}
+	}
+    //bmdMotors();
+	mav(L_MOTOR, 0);
+	mav(R_MOTOR, 0);
 }
 
-void moveStraight(int speed)		//move straight forward
+void moveStraight(int speed)
 {
 	mav(L_MOTOR, speed); 
 	mav(R_MOTOR, speed); 
 }
 
-void stopMotors()					//stops motors
+void stopMotors()
 {
 	mav(L_MOTOR, 0); 
 	mav(R_MOTOR, 0); 
 }
 
-float cmToTicks(float cmDistance) {	// return ticks the motors need to turn to go that distance
+float cmToTicks(float cmDistance) {                // return ticks the motors need to turn to go that distance
         return (float) (cmDistance * TICKS_PER_CM);        
 }
 
-void bmdMotors() {					//wait until driving motors finish moving
+void bmdMotors() {                                 //wait until driving motors finish moving
         bmd(L_MOTOR);
         bmd(R_MOTOR);   
 }
 
-//blockers have separate functions because one motor-operated and the other is servo-operated
-void lowerBlockerLeft(){			//lower left blocker and move forward 20cm (subject to change)
+void lowerBlockerLeft(){		
         mrp(L_BLOCKER_MOTOR, L_BLOCKER_MOTOR_SPEED, L_BLOCKER_MOTOR_DISTANCE);
         sleep(1);
         off(L_BLOCKER_MOTOR);
 }
 
-void lowerBlockerRight(){			//lower right blocker and move forward 20 cm (subject to change)
+void lowerBlockerRight(){		//lower right blocker and move forward 20 cm (subject to change)
         set_servo_position(0,R_BLOCKER_SERVO_POSITION);
         sleep(1);
         //when blocker is vertical: position 50
         //when blocker hits the ground and is horizontal: position 1100
 }
 
-void setStartingBlockers() {		//angle the blockers to fit in the starting box
-	set_servo_position(0,930);  	//angled value
-	mrp(L_BLOCKER_MOTOR,260,165); 	//angled value
+
+void setStartingBlockers() {         //angle the blockers to fit in the starting box
+	set_servo_position(0,930);       //angled value
+	mrp(L_BLOCKER_MOTOR,260,165);    //angled value
 }
 
-void raiseBlockers(){            	//raise higher AFTER leaving the starting box
-	set_servo_position(0,775);   	//move to highest angle possible
-	mrp(L_BLOCKER_MOTOR, 260, 25); 	//move to highest angle possible
+void raiseBlockers(){            //raise higher AFTER leaving the starting box
+	set_servo_position(0,775);       //move to highest angle possible
+	mrp(L_BLOCKER_MOTOR, 260, 25);   //move to highest angle possible
 }
 
-void calibrateGate()             	//open or close the gate holding the biofuels (ping-pong balls)
+void calibrateGate()                 //open or close the gate holding the biofuels (ping-pong balls)
 {
 	printf("Press up to open, down to close, black button to finish -- move it to the completely closed position");
-	while (1)						//loops forever until broken out of
+	while (1) 
 	{
-		if (up_button() == 1) {		//raise gate
+		if (up_button() == 1) {
 			 mrp(GATE_MOTOR,200,-20);
 		}
-		if(down_button() == 1) {	//lower gate
+		if(down_button() == 1) {
 			mrp(GATE_MOTOR,200,20);
 		}
-		if(black_button() == 1) {	//finish calibration
+		if(black_button() == 1) {
 			break;
 		}
 	}
-	mrp(GATE_MOTOR, GATE_MOTOR_SPEED,-400);	//raise gate a bit
+	mrp(GATE_MOTOR, GATE_MOTOR_SPEED,-400);
 }
 
-void gateOpen() 					//opens the gate to let out balls
+void gateOpen() 
 {
 	mrp(GATE_MOTOR, GATE_MOTOR_SPEED, GATE_OPEN_POS);
 	bmd(GATE_MOTOR);
 }
 
-void gateClose()					//closes gate
+void gateClose()
 {
 	mrp(GATE_MOTOR, GATE_MOTOR_SPEED, -GATE_OPEN_POS);
 	bmd(GATE_MOTOR);
 }
 
-void turn(int degree, int speed){ 	//turns using both wheels to rotate about the midpoint of the axle
+//        axle is 13.75 cm
+
+void turn(int degree, int speed){ 
     clear_motor_position_counter(L_MOTOR); 
     clear_motor_position_counter(R_MOTOR); 
     int ticks = degree*TICKS_PER_DEGREE; 
@@ -131,6 +148,7 @@ void turn(int degree, int speed){ 	//turns using both wheels to rotate about the
     bmdMotors();
 }
 
+
 /*
         -brief Turns legobot in an arc using the mrp command
         -param True if going left, false if going right
@@ -138,19 +156,20 @@ void turn(int degree, int speed){ 	//turns using both wheels to rotate about the
         -param outerSpeed Speed of outside wheel between 0-1000
         -param amountDegrees Tells the robot how much of the circle to turn in degrees
 */
+
+
 void turnArc(boolean leftArc, float outerRadius, float outerSpeed, float amountDegrees) 
 {
-	//axle length (LEGOBOT_DIAMETER) is 13.75 cm
     float ratio = (outerRadius - LEGOBOT_DIAMETER)/(outerRadius);
 	float outerCircumference = 2 * PI * outerRadius * (0.073343)*1.3;
     float innerCircumference = outerCircumference * ratio;
     float innerSpeed = outerSpeed * ratio; 
-	if(leftArc)									//arcing to the left
+	if(leftArc)
 	{
            mrp(L_MOTOR, innerSpeed, innerCircumference*TICKS_PER_REV*(amountDegrees/360.0)); //1020.4 is ticks per rev
            mrp(R_MOTOR, outerSpeed, outerCircumference*TICKS_PER_REV*(amountDegrees/360.0));
 	}
-    else										//arcing to the right
+    else
     {
            mrp(R_MOTOR, innerSpeed, innerCircumference*TICKS_PER_REV*(amountDegrees/360.0));
            mrp(L_MOTOR, outerSpeed, outerCircumference*TICKS_PER_REV*(amountDegrees/360.0));
@@ -159,17 +178,12 @@ void turnArc(boolean leftArc, float outerRadius, float outerSpeed, float amountD
 }
 //Inner: 3+, 4.5
 
+//////Marked for Deletion
 /*  diameter of a wheel = 4.32
 	circumference = pi * 4.32 = 13.571
 	length of whole rotation with one wheel at center = 3.14159* 2 * 13.75 <- dist btw center of two wheels
 	revs/circle = 6.33 */
 
-/*
-        -brief Rotates legobot using a wheel as a pivot point
-        -param True if using left wheel as pivot, false if using right wheel
-        -param degrees Tells robot how much of the circle to turn in degrees
-		-param speed Sets speed of the wheel that moves
-*/
 void pivot(boolean aroundLeftWheel, float degrees, int speed) 
 {
 	int ticks = degrees /360 * 6646.166464987; // fraction of circle's circumference / ticks for a whole circle
@@ -184,3 +198,6 @@ void pivot(boolean aroundLeftWheel, float degrees, int speed)
 	}
 }	
 //6.366 = revs per big circle
+
+
+
--- trunk/orangeelephants/2011/Legobot/legobotMain.c
+++ trunk/orangeelephants/2011/Legobot/legobotMain.c
@@ -2,95 +2,53 @@
 #include "legobotConstants.h"
 #include "boolean.h"
 
-
 // BLOCKERS NEED TO START ON GROUND BEFORE ROUND, THEN CALL setStartingBlockers()
-
 int main() {
-/*	calibrateGate();
-	sleep(1);
-	moveToDistance(10,SLOW_SPEED);
-	gateOpen();
-	moveToDistance(10,SLOW_SPEED);
-	gateClose();
-	turnArc(1, 30, SLOW_SPEED, 90);
-*/
-	//THIS IS THE START OF THE ROUTINE... DONT DELETE IT
 	calibrateGate();
-	setStartingBlockers();FAST_SPEED
+	setStartingBlockers();
 	printf("press a button to start \n ");
 	while(1) {
 			if(a_button() == 1) {
-					break;
+				break;
 			}
+			if(b_button() == 1) {
+				moveToDistance(200, SLOW_SPEED);
+				sleep(1);
+				moveToDistance(-200, SLOW_SPEED);
+			}
+				
 	}
 	//code for right side
 	
-    raiseBlockers();
-    moveToDistance(48.5,NORMAL_SPEED);
-    sleep(0.5);
-    moveToDistance(3.5,SLOW_SPEED);
-    moveToDistance(-20,FAST_SPEED);
-    turnArc(0,26,SLOW_SPEED,60);
-    turnArc(1,30,SLOW_SPEED,52); //52 is compensation
-	moveToDistance(105,FAST_SPEED);				
+    //raiseBlockers();
+    moveToDistance(52, FAST_SPEED);//moves forward, pushing away blocks and collecting ping pong balls
+	sleep(.7);
+    moveToDistance(-20, FAST_SPEED);//moves back to move around blocks
+    turnArc(0, 23, FAST_SPEED, 60);//arcs around blocks
+    turnArc(1, 30, FAST	_SPEED, 54); 
+	moveToDistance(120, FAST_SPEED); //hits North wall near the drop zone				
 	
 	
 	
-	moveToDistance(-25,FAST_SPEED);
-	turn(-90,400);
-	moveToDistance(50,FAST_SPEED);
-	moveToDistance(-9,FAST_SPEED); //tested: moving back 7cm lines up with the wall best
-	turn(-93,400);
-	moveToDistance(30,FAST_SPEED);
-	sleep(1);
-	moveToDistance(3.5,SLOW_SPEED); 
+	moveToDistance(-25,FAST_SPEED);//backs up to allow for turn
+	turn(-90,SLOW_SPEED);//turns to face West
+	moveToDistance(54, FAST_SPEED); //align with pvc on West
+	moveToDistance(-9, FAST_SPEED); //backs up to allow for turn
+	turn(-93, SLOW_SPEED);//turns to face blocks, South
+	moveToDistance(30, FAST_SPEED); //moves blocks out of the way
+	moveToDistance(3.5, FAST_SPEED);//got all the biofuels
+	turnArc(1, 50, -SLOW_SPEED, -10);
+	
+	moveToDistance(-55,FAST_SPEED);
+	moveToDistance(4, FAST_SPEED);
 	turnArc(1,50,-SLOW_SPEED,-10);
 	
-	moveToDistance(-55,SLOW_SPEED);
-	turnArc(1,20,SLOW_SPEED,90); //100 is compensation
-	moveToDistance(90,FAST_SPEED);
-	pivot(0,-101,SLOW_SPEED); //runs into wall. 96 is compensation
-	moveToDistance(55,FAST_SPEED);
+	turnArc(1, 20, SLOW_SPEED, 93); //100 is compensation
+	moveToDistance(100, FAST_SPEED);
+	pivot(0, -99, SLOW_SPEED); //runs into wall. 96 is compensation
+	moveToDistance(55, FAST_SPEED);
 	moveStraight(200);
 	gateOpen();
-	
-	
-	
-	/*
-	moveToDistance(50, FAST_SPEED);
-	sleep(2);
-	turnArc(1, 20, SLOW_SPEED, 45);
-	sleep(2);
-	turnArc(1, 40, SLOW_SPEED, 140);
-	*/
-	
-	//code for left side
-	/*
-	moveToDistance(23,SLOW_SPEED);
-    raiseBlockers();
-    moveToDistance(25.5,NORMAL_SPEED);
-    sleep(0.5);
-    moveToDistance(3.5,SLOW_SPEED);
-    moveToDistance(-20,FAST_SPEED);
-    turnArc(1,26,SLOW_SPEED,60);
-    turnArc(0,30,SLOW_SPEED,52);
-	moveToDistance(105,FAST_SPEED);
-	moveToDistance(-20,FAST_SPEED);
-	turn(90,SLOW_SPEED);
-	moveToDistance(22,FAST_SPEED);
-	moveToDistance(-6,FAST_SPEED); //tested: moving back 6 lines up with the wall best
-	turn(90,SLOW_SPEED);
-	moveToDistance(30,NORMAL_SPEED);
-	sleep(0.5);
-	moveToDistance(-20,NORMAL_SPEED);
-	turnArc(0,26,SLOW_SPEED,100);
-	moveToDistance(80,FAST_SPEED);
-	moveToDistance(-6,NORMAL_SPEED);
-	turn(90,NORMAL_SPEED);
-	moveToDistance(50,FAST_SPEED);
-	gateOpen();
-	*/
 }
-
 
 
