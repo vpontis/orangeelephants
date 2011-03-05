1f9172b08e2fe939bc3fbe49f0f315b91f2b5840
ericsliu@gmail.com <ericsliu@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-05 22:47:39 +0000

    Commented code, made format more uniform.

--- orangeelephants/2011/Legobot/legobotFunctions.c
+++ orangeelephants/2011/Legobot/legobotFunctions.c
@@ -9,7 +9,7 @@ void legobotAccel(int initSpeed,int finalSpeed){
         int increm = (finalSpeed-initSpeed)/20;
         int speed = initSpeed;
         
-        float time = abs((finalSpeed-initSpeed)/2500);  //delay is .1 sec for accel from 0-500
+        float time = abs((finalSpeed-initSpeed)/2500);	//delay is .1 sec for accel from 0-500
         
         if(speed<finalSpeed)		//if you are accelerating
         {
@@ -21,7 +21,7 @@ void legobotAccel(int initSpeed,int finalSpeed){
      
 				}
 		}		
-        else				//if you are decelerating
+        else						//if you are decelerating
         {
                 while(speed>finalSpeed)
                 {
@@ -35,7 +35,6 @@ void legobotAccel(int initSpeed,int finalSpeed){
         mav(R_MOTOR, finalSpeed);
         
 }
-
 
 void moveToDistance(float distance, int speed) {
 	clear_motor_position_counter(L_MOTOR); 
@@ -57,43 +56,43 @@ void stopMotors()
 	mav(R_MOTOR, 0); 
 }
 
-float cmToTicks(float cmDistance) {                // return ticks the motors need to turn to go that distance
+float cmToTicks(float cmDistance) {	// return ticks the motors need to turn to go that distance
         return (float) (cmDistance * TICKS_PER_CM);        
 }
 
-void bmdMotors() {                                 //wait until driving motors finish moving
+void bmdMotors() {					//wait until driving motors finish moving
         bmd(L_MOTOR);
         bmd(R_MOTOR);   
 }
 
-void lowerBlockerLeft(){		
+//blockers have separate functions because one motor-operated and the other is servo-operated
+void lowerBlockerLeft(){			//lower left blocker and move forward 20cm (subject to change)
         mrp(L_BLOCKER_MOTOR, L_BLOCKER_MOTOR_SPEED, L_BLOCKER_MOTOR_DISTANCE);
         sleep(1);
         off(L_BLOCKER_MOTOR);
 }
 
-void lowerBlockerRight(){		//lower right blocker and move forward 20 cm (subject to change)
+void lowerBlockerRight(){			//lower right blocker and move forward 20 cm (subject to change)
         set_servo_position(0,R_BLOCKER_SERVO_POSITION);
         sleep(1);
         //when blocker is vertical: position 50
         //when blocker hits the ground and is horizontal: position 1100
 }
 
-
-void setStartingBlockers() {         //angle the blockers to fit in the starting box
-	set_servo_position(0,930);       //angled value
-	mrp(L_BLOCKER_MOTOR,260,165);    //angled value
+void setStartingBlockers() {		//angle the blockers to fit in the starting box
+	set_servo_position(0,930);  	//angled value
+	mrp(L_BLOCKER_MOTOR,260,165); 	//angled value
 }
 
-void raiseBlockers(){            //raise higher AFTER leaving the starting box
-	set_servo_position(0,775);       //move to highest angle possible
-	mrp(L_BLOCKER_MOTOR, 260, 25);   //move to highest angle possible
+void raiseBlockers(){            	//raise higher AFTER leaving the starting box
+	set_servo_position(0,775);   	//move to highest angle possible
+	mrp(L_BLOCKER_MOTOR, 260, 25); 	//move to highest angle possible
 }
 
-void calibrateGate()                 //open or close the gate holding the biofuels (ping-pong balls)
+void calibrateGate()             	//open or close the gate holding the biofuels (ping-pong balls)
 {
 	printf("Press up to open, down to close, black button to finish -- move it to the completely closed position");
-	while (1) 
+	while (1)						//loops forever until broken out of
 	{
 		if (up_button() == 1) {
 			 mrp(GATE_MOTOR,200,-20);
@@ -108,19 +107,17 @@ void calibrateGate()                 //open or close the gate holding the biofue
 	mrp(GATE_MOTOR, GATE_MOTOR_SPEED,-400);
 }
 
-void gateOpen() 
+void gateOpen() 					//opens the gate to let out balls
 {
 	mrp(GATE_MOTOR, GATE_MOTOR_SPEED, GATE_OPEN_POS);
 	bmd(GATE_MOTOR);
 }
 
-void gateClose()
+void gateClose()					//closes gate
 {
 	mrp(GATE_MOTOR, GATE_MOTOR_SPEED, -GATE_OPEN_POS);
 	bmd(GATE_MOTOR);
 }
-
-//        axle is 13.75 cm
 
 void turn(int degree, int speed){ 
     clear_motor_position_counter(L_MOTOR); 
@@ -134,7 +131,6 @@ void turn(int degree, int speed){
     bmdMotors();
 }
 
-
 /*
         -brief Turns legobot in an arc using the mrp command
         -param True if going left, false if going right
@@ -142,20 +138,19 @@ void turn(int degree, int speed){
         -param outerSpeed Speed of outside wheel between 0-1000
         -param amountDegrees Tells the robot how much of the circle to turn in degrees
 */
-
-
 void turnArc(boolean leftArc, float outerRadius, float outerSpeed, float amountDegrees) 
 {
+	//axle length (LEGOBOT_DIAMETER) is 13.75 cm
     float ratio = (outerRadius - LEGOBOT_DIAMETER)/(outerRadius);
 	float outerCircumference = 2 * PI * outerRadius * (0.073343)*1.3;
     float innerCircumference = outerCircumference * ratio;
     float innerSpeed = outerSpeed * ratio; 
-	if(leftArc)
+	if(leftArc)									//arcing to the left
 	{
            mrp(L_MOTOR, innerSpeed, innerCircumference*TICKS_PER_REV*(amountDegrees/360.0)); //1020.4 is ticks per rev
            mrp(R_MOTOR, outerSpeed, outerCircumference*TICKS_PER_REV*(amountDegrees/360.0));
 	}
-    else
+    else										//arcing to the right
     {
            mrp(R_MOTOR, innerSpeed, innerCircumference*TICKS_PER_REV*(amountDegrees/360.0));
            mrp(L_MOTOR, outerSpeed, outerCircumference*TICKS_PER_REV*(amountDegrees/360.0));
@@ -164,12 +159,17 @@ void turnArc(boolean leftArc, float outerRadius, float outerSpeed, float amountD
 }
 //Inner: 3+, 4.5
 
-//////Marked for Deletion
 /*  diameter of a wheel = 4.32
 	circumference = pi * 4.32 = 13.571
 	length of whole rotation with one wheel at center = 3.14159* 2 * 13.75 <- dist btw center of two wheels
 	revs/circle = 6.33 */
 
+/*
+        -brief Rotates legobot using a wheel as a pivot point
+        -param True if using left wheel as pivot, false if using right wheel
+        -param degrees Tells robot how much of the circle to turn in degrees
+		-param speed Sets speed of the wheel that moves
+*/
 void pivot(boolean aroundLeftWheel, float degrees, int speed) 
 {
 	int ticks = degrees /360 * 6646.166464987; // fraction of circle's circumference / ticks for a whole circle
@@ -184,6 +184,3 @@ void pivot(boolean aroundLeftWheel, float degrees, int speed)
 	}
 }	
 //6.366 = revs per big circle
-
-
-
