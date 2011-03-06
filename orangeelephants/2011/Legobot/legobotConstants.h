d3d6979547f2d8961f8fe3f509a2ee402c74f4c3
pwd.webmaster@gmail.com <pwd.webmaster@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-06 02:42:45 +0000

    Everything works @ 950 FAST_SPEED and full charge (MUST HAVE full charge)

--- orangeelephants/2011/Legobot/legobotConstants.h
+++ orangeelephants/2011/Legobot/legobotConstants.h
@@ -13,10 +13,6 @@ const int GATE_MOTOR = 1;
 //Servo ports
 const int R_BLOCKER_SERVO = 0;
  
-//Speeds
-const int FAST_SPEED = 950;
-const int MED_SPEED = 700;
-const int SLOW_SPEED = 500;
 
 //Blocker speed(motor) and position(servo)
 const int L_BLOCKER_MOTOR_SPEED = 100; //motor
