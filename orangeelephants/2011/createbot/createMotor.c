544554e66fa702d90c8bc4df94581396e5769ed5
prithvi.undavalli@gmail.com <prithvi.undavalli@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-07 04:10:31 +0000

    LIKE OMYYYGGGOOOOD. I fixed and perfected moveToDistAccel. We use it. Everywhere and in every line.

--- orangeelephants/2011/createbot/createMotor.c
+++ orangeelephants/2011/createbot/createMotor.c
@@ -11,7 +11,7 @@ void accel(int initSpeed, int finalSpeed)
 	int speed = initSpeed;
 	int increm = (finalSpeed-initSpeed)/20;
 	
-	float time = abs((finalSpeed-initSpeed)/2500);//delay is .1 sec for accel from 0-500
+	float time = abs((finalSpeed-initSpeed)/2000);//delay is .1 sec for accel from 0-500
 	
 	if(speed<finalSpeed)//if you are accelerating
 	{
@@ -42,6 +42,41 @@ void createDrive(int finalSpeed)
 	accel(initSpeed, finalSpeed);
 }
 
+
+
+void modAccel(int distance, int highSpeed){
+	
+	
+	int speed = get_create_requested_velocity(.1);
+	int increm = (highSpeed-speed)/20;
+	
+	float time = abs(((highSpeed-speed))/2500*(distance/500));//delay is .1 sec for accel from 0-500
+	
+	if(speed<highSpeed)//if you are accelerating
+	{
+		while(speed<highSpeed)
+		{
+			speed += increm; 
+			create_drive_straight(speed);
+			sleep(time);
+		}
+	}
+	else//if you are decelerating
+	{
+		while(speed>highSpeed)
+		{
+			speed += increm; 
+			create_drive_straight(speed);
+			sleep(time);
+		}
+	}
+	
+	create_drive_straight(highSpeed);
+	
+	
+	
+	
+	}
 /** \brief Moves the robot a distance at a constant speed
 
 	Does not work well for very short distances.
@@ -65,137 +100,115 @@ void moveToDist(int distance, int speed)
 	}
 }
 
-
-/** \brief Moves the robot a distance at a changing speed
-
-	Does not work well for very short distances.
-	\param distance Desired distance in mm
-	\param initSpeed 
-	\param finalSpeed 
-*/
-
-void moveToDistanceAccel(int distance, int finalSpeed) 
-{ 
-	set_create_distance(0); 
-	
-	double numIncrements = 4.;
-	double increment = finalSpeed/numIncrements;
-	
-	int currSpeed = increment;
-	double totalDistance = 0;
-	createDrive(currSpeed);
-	if(distance > 0) {
-		while((totalDistance) < (distance/numIncrements)) {   // WHY divide by 4? DONT FUCKING KNOW.
-			set_create_distance(0);
-			while(get_create_distance(.1) < (distance/(numIncrements))){}
-			totalDistance += (distance/(numIncrements));
-			currSpeed += increment;
-			createDrive(currSpeed);
-		}
-		
-		totalDistance = 0;
-		while((totalDistance) < (distance/numIncrements)) {   
-			set_create_distance(0);
-			while(get_create_distance(.1) < (distance/numIncrements)) {}
-			totalDistance += (distance/(numIncrements));
-			currSpeed -= increment;
-			createDrive(currSpeed);
-		}
-		
-	}
-	else{
-		while((totalDistance) > (distance/numIncrements)) { // WHY divide by 4? DONT FUCKING KNOW.
-			//WTF I DON'T KNOW WHY THIS WORKS BUT IT DOES.
-			set_create_distance(0);
-			while(get_create_distance(.1) > (distance/numIncrements)) {}
-			totalDistance += (distance/numIncrements);
-			currSpeed += increment;
-			createDrive(currSpeed);
-		}
-		totalDistance = 0;
-		while((totalDistance) > (distance/numIncrements)) {   //WTF I DON'T KNOW WHY THIS WORKS BUT IT DOES
-			set_create_distance(0);
-			while(get_create_distance(.1) > (distance/numIncrements)) {}
-			totalDistance += (distance/numIncrements);
-			currSpeed -= increment;
-			createDrive(currSpeed);
-		}
-	}
-	create_stop();
-	
-}
-
-
-void turn(float deg, int vel)	{
-	deg = -deg; 
-	set_create_total_angle(0);
-	if(deg > 0)	{
-		//create_spin_CCW(vel);
-		create_drive_direct(vel, -vel); //turns CCW
-		while(get_create_total_angle(.05) < deg){}
-	}
-	else{	
-		//create_spin_CW(vel);
-		create_drive_direct(-vel, vel);
-		while(get_create_total_angle(.05) > deg){}
-	}
-	create_stop();
-}
-
 //Optimized at d = 90, v = 200. Test at other values. 
 void smoothTurn(float deg, int finalVel)	{
 	deg = -deg;
 	set_create_total_angle(0);
-	float compRatio = 0.81; //.00405*finalVel*(standardVel/finalVel)
+	float compRatio = 1.7; //.00405*finalVel*(standardVel/finalVel)
 	
 	int increment = finalVel/10;
 	int currVel = increment;
 	if(deg > 0){
 		//CCW: +deg, +vel
-		while(get_create_total_angle(.1) < (compRatio*deg))	{
+		while(get_create_total_angle(.1) < 0.5*(compRatio*deg))	{
 			while(currVel < finalVel)
 			{
 				create_drive_direct(currVel, -currVel);
 				currVel += increment;
-				sleep(.15*(deg/90));
+				sleep(.15);
 			}
 			currVel = finalVel;
 			create_drive_direct(currVel, -currVel);
 		}
-		while(get_create_total_angle(.1) < compRatio*deg){
+		
+		set_create_total_angle(0);
+		/*
+		while(get_create_total_angle(.1) < 0.5*(compRatio*deg)){
 			while(currVel > increment)
 			{
 				create_drive_direct(currVel, -currVel);
 				currVel -= increment;
-				sleep(.15*(deg/90));
+				sleep(.15);
 			}
 			currVel = 0;
 			create_drive_direct(currVel, -currVel);
-		}
+		}*/
 	}
 	else{
-		while(get_create_total_angle(.1) > (compRatio*deg))	{
+		while(get_create_total_angle(.1) > 0.5*(compRatio*deg))	{
 			while(currVel < finalVel)
 			{
 				create_drive_direct(-currVel, currVel);
 				currVel += increment;
-				sleep(.15*(deg/90));
+				sleep(.15);
 			}
 			currVel = finalVel;
 			create_drive_direct(-currVel, currVel);
 		}
-		while(get_create_total_angle(.1) > compRatio*deg){
+		set_create_total_angle(0);
+		/*
+		while(get_create_total_angle(.1) > 0.5*(compRatio*deg)){
 			while(currVel > increment)
 			{
 				create_drive_direct(-currVel, currVel);
 				currVel -= increment;
-				sleep(.15*(deg/90));
+				sleep(.15);
 			}
 			currVel = 0;
 			create_drive_direct(-currVel, currVel);
-		}
+		}*/
 	}
 	create_stop();
 	
 }
 
+void moveToDistAccel(int distance, int finalSpeed) 
+{ 
+	distance = -distance;
+	set_create_distance(0); 
+	double numIncrements = 20.;
+	double increment = finalSpeed/numIncrements;
+	double initSpeed = increment;
+	int currSpeed = initSpeed;
+	double totalDistance = 0;
+	create_drive_straight(currSpeed);
+	if(distance > 0) {
+		while((totalDistance) < (distance/2)) {   // WHY divide by 4? DONT FUCKING KNOW.
+			set_create_distance(0);
+			while(get_create_distance(.1) < (distance/(numIncrements))){}
+			totalDistance += (distance/(numIncrements));
+			currSpeed += increment;
+			create_drive_straight(currSpeed);
+		}
+		
+		totalDistance = 0;
+		while((totalDistance) < (distance/2)) {   
+			set_create_distance(0);
+			while(get_create_distance(.1) < (distance/numIncrements)) {}
+			totalDistance += (distance/(numIncrements));
+			currSpeed -= increment;
+			create_drive_straight(currSpeed);
+		}
+		
+	}
+	else{
+		while((totalDistance) > (distance/2)) { // WHY divide by 4? DONT FUCKING KNOW.
+			//WTF I DON'T KNOW WHY THIS WORKS BUT IT DOES.
+			set_create_distance(0);
+			while(get_create_distance(.1) > (distance/numIncrements)) {}
+			totalDistance += (distance/numIncrements);
+			currSpeed += increment;
+			create_drive_straight(-currSpeed);
+		}
+		totalDistance = 0;
+		while((totalDistance) > (distance/2)) {   //WTF I DON'T KNOW WHY THIS WORKS BUT IT DOES
+			set_create_distance(0);
+			while(get_create_distance(.1) > (distance/numIncrements)) {}
+			totalDistance += (distance/numIncrements);
+			currSpeed -= increment;
+			create_drive_straight(-currSpeed);
+		}
+	}
+	create_stop();
+	
+}
