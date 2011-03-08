c2d60d542987671a9835cfe41e85697b565d9229
pwd.webmaster@gmail.com <pwd.webmaster@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-08 04:25:02 +0000

    2 pivot beginning. haven't tested fully

--- trunk/orangeelephants/2011/Legobot/legobotMain.c
+++ trunk/orangeelephants/2011/Legobot/legobotMain.c
@@ -19,24 +19,28 @@ int main() {
 			}
 	}
 	//code for right side
-
+	
+	
     //raiseBlockers();                 
     moveToDistance(52, FAST_SPEED);//moves forward, pushing away blocks and collecting ping pong balls
 	sleep(.7);
-    moveToDistance(-20, FAST_SPEED);//moves back to move around blocks
+    moveToDistance(-6, FAST_SPEED);//moves back to move around blocks
 	
-    turnArc(0, 23, FAST_SPEED, 60);//arcs around blocks
+	pivot(0,-90,FAST_SPEED);
+	moveToDistance(-5,FAST_SPEED);
+	pivot(1,-87,FAST_SPEED);
+    //turnArc(0, 23, FAST_SPEED, 60);//arcs around blocks
 	
 	//CALIBRATE THIS VALUE!!!
-    pivot(1,79,FAST_SPEED); //NEED TO CALIBRATE EVERY DAY; currently 79 degrees
+    //pivot(1,86,FAST_SPEED); //NEED TO CALIBRATE EVERY DAY; currently 88 degrees
 	
 	//turnArc(1, 30, FAST_SPEED, 52);
-	moveToDistance(145, FAST_SPEED); //hits North wall near the drop zone				
+	moveToDistance(160, FAST_SPEED); //hits North wall near the drop zone				
 	
 	moveToDistance(-25,FAST_SPEED);//backs up to allow for turn
-	turn(-90,SLOW_SPEED);//turns to face West
+	turn(-93,SLOW_SPEED);//turns to face West
 	moveToDistance(65,FAST_SPEED); //align with pvc on West
-	moveToDistance(-10,FAST_SPEED); //backs up to allow for turn. 10 is best.
+	moveToDistance(-9,FAST_SPEED); //backs up to allow for turn. 10 is best.
 	
 	//CALIBRATE THIS VALUE!!!
 	turn(-100,SLOW_SPEED);//turns to face blocks, South
@@ -51,26 +55,35 @@ int main() {
 	
 	turnArc(1, 20, FAST_SPEED, 92); //100 is compensation
 	moveToDistance(100, FAST_SPEED);
-	pivot(0, -94, FAST_SPEED); //runs into wall. 96 is compensation
+	pivot(0, -91, FAST_SPEED); //runs into wall. currently 91
 	moveToDistance(55, FAST_SPEED);
 	moveStraight(200);
 	gateOpen();
 	gateClose();
-	
 
 	moveToDistance(-50,FAST_SPEED);
 	pivot(1, -90, FAST_SPEED);
 	moveToDistance(30,FAST_SPEED);
-	moveToDistance(-100,FAST_SPEED);
+	moveToDistance(-22,FAST_SPEED);
+	pivot(0,-90,FAST_SPEED);
+	moveToDistance(-45,FAST_SPEED);
+	pivot(1,-90,FAST_SPEED);
+	moveToDistance(-60,FAST_SPEED);
+	moveToDistance(-100,SUPER_SPEED);
 	
-	pivot(1, -120, FAST_SPEED);
-	moveToDistance(25,FAST_SPEED);
+	/*
+	pivot(0, -55, FAST_SPEED);
+	moveToDistance(-20,FAST_SPEED);
 	pivot(1,-60,FAST_SPEED);
-	moveToDistance(150,SUPER_SPEED);
+	
+	moveToDistance(30,FAST_SPEED);
+	sleep(0.5);
+	moveToDistance(-120,SUPER_SPEED);
 	
 	lowerBlockerLeft();
 	lowerBlockerRight();
-	
+	*/
+
 }
 
 
