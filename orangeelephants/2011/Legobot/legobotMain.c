e74fc850bb9e1e601292fbff83fc8a5f1514551f
dheeraj.navani@gmail.com <dheeraj.navani@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-10 00:02:30 +0000

    Added side B. Needs callibration.

--- orangeelephants/2011/Legobot/legobotMain.c
+++ orangeelephants/2011/Legobot/legobotMain.c
@@ -8,7 +8,7 @@ int main() {
 	calibrateGate();
 	setStartingBlockers();
 	
-	printf("Press 'A' to start \n");
+	printf("Press 'A' to start if you are on Side A.\nPress 'B' if you are on Side B.\n");
 	
 	while(1) 
 	{
@@ -45,7 +45,7 @@ int main() {
 				turnArc(1, 50, -SLOW_SPEED, -10); //arcs back and hits north PVC
 				moveToDistance(-55,FAST_SPEED); //aligns with north PVC
 				moveToDistance(4, FAST_SPEED); //moves slightly forward to leave room for pivot
-				pivot(1, 108, FAST_SPEED); //pivots to face final east PVC
+				pivot(1, 119, FAST_SPEED); //pivots to face final east PVC
 				moveToDistance(100, FAST_SPEED); //moves and aligns with east PVC
 				pivot(0, -93, FAST_SPEED); //pivots to face drop zone
 				moveToDistance(100, FAST_SPEED); //drives to and aligns with drop zone
@@ -74,8 +74,57 @@ int main() {
 			//LEFT ****DONT MOD THIS YET****
 			if(b_button() == 1)
 			{
-				break;
-			}
-	}
+			//LEG 1: collects ping pong balls @ first station
 	
+			moveToDistance(52, FAST_SPEED);
+			sleep(.7);
+		
+			//LEG 2: navigates to north PVC
+		
+			moveToDistance(-6, FAST_SPEED); //moves back to avoid tampering the red towers	
+			pivot(1,-90,FAST_SPEED); //pivots -90 degrees CCW
+			moveToDistance(-7,FAST_SPEED);//moves back to avoid pivoting on black tape
+			pivot(0,-86,FAST_SPEED); //pivots -90 degrees CW
+			moveToDistance(160, FAST_SPEED); //moves to and hits north PVC
+			
+			//LEG 3: collects ping pong balls @ second station
+			
+			moveToDistance(-25,FAST_SPEED); //backs up to allow for turn
+			pivot(0,96,FAST_SPEED); //pivots to face west
+			moveToDistance(65,FAST_SPEED); //aligns with west PVC
+			moveToDistance(-9,FAST_SPEED); //backs up to allow for turn
+			pivot(1,-100,FAST_SPEED); //pivots to face south PVC & ping pong balls
+			moveToDistance(25, FAST_SPEED); //collects all biofuels
+			sleep(.7);
+		
+			//LEG 4: drop all biofuels
+		
+			turnArc(1, 50, -SLOW_SPEED, 10); //arcs back and hits north PVC
+			moveToDistance(-55,FAST_SPEED); //aligns with north PVC
+			moveToDistance(4, FAST_SPEED); //moves slightly forward to leave room for pivot
+			pivot(0, 100, FAST_SPEED); //pivots to face final east PVC
+			moveToDistance(100, FAST_SPEED); //moves and aligns with east PVC
+			pivot(1, -91, FAST_SPEED); //pivots to face drop zone
+			moveToDistance(100, FAST_SPEED); //drives to and aligns with drop zone
+			gateOpen(); //drops all biofuels
+			
+			//LEG 5: navigate and drop blockers
+			
+			gateClose();
+			moveToDistance(-50,FAST_SPEED);
+			pivot(0, -90, FAST_SPEED);
+			
+			moveToDistance(30,FAST_SPEED);
+			moveToDistance(-22,FAST_SPEED);
+			pivot(1,-90,FAST_SPEED);
+			
+			moveToDistance(-35,FAST_SPEED);
+			pivot(0,-100,FAST_SPEED);
+			
+			moveToDistance(-60,FAST_SPEED);
+			moveToDistance(-100,SUPER_SPEED);
+			break;
+			}
+					
+	}		
 }
