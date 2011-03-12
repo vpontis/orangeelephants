7643937a32843b95d352575ac7eed4abf5c5781c
eLpWnZoR007@gmail.com <eLpWnZoR007@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-12 02:53:42 +0000

    changed values. side a working at the moment.

--- trunk/orangeelephants/2011/Legobot/legobotMain.c
+++ trunk/orangeelephants/2011/Legobot/legobotMain.c
@@ -8,7 +8,7 @@ int main() {
 	calibrateGate();
 	setStartingBlockers();
 	
-	printf("Press 'A' to start if you are on Side A.\nPress 'B' if you are on Side B.\n");
+	printf("Press 'A' to start if you are on Side A.\nPress 'B' to start if you are on Side B.\n");
 	
 	while(1) 
 	{
@@ -16,18 +16,18 @@ int main() {
 			{
 				//LEG 1: collects ping pong balls @ first station
 	
-				moveToDistance(60, FAST_SPEED);
+				moveToDistance(55, FAST_SPEED);
 				sleep(.7);
 				
 				//LEG 2: navigates to north PVC
 				
-				moveToDistance(-6, FAST_SPEED); //moves back to avoid tampering the red towers	
+				moveToDistance(-7, FAST_SPEED); //moves back to avoid tampering the red towers	
 				pivot(0,-90,FAST_SPEED); //pivots -90 degrees CCW
 				moveToDistance(-7,FAST_SPEED);//moves back to avoid pivoting on black tape
-				pivot(1,-150,FAST_SPEED); //pivots -90 degrees CW
+				pivot(1,-95,FAST_SPEED); //pivots -90 degrees CW
 				moveToDistance(125, FAST_SPEED); //moves to and hits north PVC
 				
-				pivot(1,45,FAST_SPEED); //final pivot
+				pivot(1,20,FAST_SPEED); //final pivot - old is 45
 				moveToDistance(50,FAST_SPEED);
 				
 				//LEG 3: collects ping pong balls @ second station
@@ -48,7 +48,7 @@ int main() {
 				pivot(1, 119, FAST_SPEED); //pivots to face final east PVC
 				moveToDistance(100, FAST_SPEED); //moves and aligns with east PVC
 				pivot(0, -93, FAST_SPEED); //pivots to face drop zone
-				moveToDistance(100, FAST_SPEED); //drives to and aligns with drop zone
+				moveToDistance(90, FAST_SPEED); //drives to and aligns with drop zone
 				gateOpen(); //drops all biofuels
 				
 				//LEG 5: navigate and drop blockers
@@ -74,35 +74,35 @@ int main() {
 			{
 				//LEG 1: collects ping pong balls @ first station
 		
-				moveToDistance(52, FAST_SPEED);
+				moveToDistance(55, FAST_SPEED);
 				sleep(.7);
 			
 				//LEG 2: navigates to north PVC
 			
-				moveToDistance(-6, FAST_SPEED); //moves back to avoid tampering the red towers	
+				moveToDistance(-5, FAST_SPEED); //moves back to avoid tampering the red towers	
 				pivot(1,-90,FAST_SPEED); //pivots -90 degrees CCW
 				moveToDistance(-7,FAST_SPEED);//moves back to avoid pivoting on black tape
-				pivot(0,-86,FAST_SPEED); //pivots -90 degrees CW
+				pivot(0,-87,FAST_SPEED); //pivots -90 degrees CW
 				moveToDistance(160, FAST_SPEED); //moves to and hits north PVC
 				
 				//LEG 3: collects ping pong balls @ second station
 				
 				moveToDistance(-25,FAST_SPEED); //backs up to allow for turn
-				pivot(0,96,FAST_SPEED); //pivots to face west
+				turn(96,SLOW_SPEED); //pivots to face west
 				moveToDistance(65,FAST_SPEED); //aligns with west PVC
 				moveToDistance(-9,FAST_SPEED); //backs up to allow for turn
-				pivot(1,-100,FAST_SPEED); //pivots to face south PVC & ping pong balls
+				turn(102,SLOW_SPEED); //pivots to face south PVC & ping pong balls
 				moveToDistance(25, FAST_SPEED); //collects all biofuels
 				sleep(.7);
 			
 				//LEG 4: drop all biofuels
 			
-				turnArc(1, 50, -SLOW_SPEED, 10); //arcs back and hits north PVC
+				turnArc(1, 50, -SLOW_SPEED, -14); //arcs back and hits north PVC
 				moveToDistance(-55,FAST_SPEED); //aligns with north PVC
 				moveToDistance(4, FAST_SPEED); //moves slightly forward to leave room for pivot
 				pivot(0, 100, FAST_SPEED); //pivots to face final east PVC
 				moveToDistance(100, FAST_SPEED); //moves and aligns with east PVC
-				pivot(1, -91, FAST_SPEED); //pivots to face drop zone
+				pivot(1, -92, FAST_SPEED); //pivots to face drop zone
 				moveToDistance(100, FAST_SPEED); //drives to and aligns with drop zone
 				gateOpen(); //drops all biofuels
 				
@@ -117,7 +117,7 @@ int main() {
 				pivot(1,-90,FAST_SPEED);
 				
 				moveToDistance(-35,FAST_SPEED);
-				pivot(0,-100,FAST_SPEED);
+				pivot(0,-90,FAST_SPEED);
 				
 				moveToDistance(-60,FAST_SPEED);
 				moveToDistance(-100,SUPER_SPEED);
