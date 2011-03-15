020b8670a577dc7a3eff1f9c39b689ce9e54a57f
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-15 05:16:07 +0000

    Not done for the night

--- trunk/orangeelephants/2011/createbot/createMotor.h
+++ trunk/orangeelephants/2011/createbot/createMotor.h
@@ -6,9 +6,11 @@ void createDrive(int finalSpeed);
 
 void moveToDistance(int distance, int speed);
 void moveToDistanceAccel(int distance, int finalSpeed);
+void moveToVictor(int distance, int speed);
 
 void turn(float deg, int vel);
 void smoothTurn(float deg, int finalVel);
 void moveStraight(int speed);
+
 
 #endif
