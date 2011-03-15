885aacf4a3c139519b57fe0fddadb55baeab7dac
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-15 02:50:22 +0000

    Working LegoBot code.

--- orangeelephants/2011/Legobot/legobotMain.c
+++ orangeelephants/2011/Legobot/legobotMain.c
@@ -22,13 +22,12 @@ int main() {
 				sleep(.7);
 				
 				//LEG 2: navigates to north PVC
-			
 				moveToDistance(-2, FAST_SPEED); //moves back to avoid tampering the red towers	
 				pivot(0,-90,FAST_SPEED); //pivots -90 degrees CW
 				moveToDistance(-7,FAST_SPEED);//moves back to avoid pivoting on black tape
 				pivot(1,-80,FAST_SPEED); //pivots -65 degrees CCW
 				turnArc(0,98,FAST_SPEED,10);
-				pivot(1,18,FAST_SPEED);
+				//pivot(1,18,FAST_SPEED);
 				sleep(5);
 				moveToDistance(120,FAST_SPEED);
 				
@@ -36,8 +35,7 @@ int main() {
 				//sleep(5); //to wait for the create to go past
 				//moveToDistance(80,FAST_SPEED); //finish distance
 				
-				//LEG 3: collects ping pong balls @ second station
-				
+				//LEG 3: collects ping pong balls @ second station				
 				moveToDistance(-22,FAST_SPEED); //backs up to allow for turn
 				turn(-90,SLOW_SPEED); //pivots to face west PVC
 				moveToDistance(35,FAST_SPEED); //aligns with west PVC
@@ -47,7 +45,6 @@ int main() {
 				sleep(.7);
 				
 				//LEG 4: drop all biofuels
-			
 				turnArc(1, 50, -MED_SPEED, -4); //arcs back and hits north PVC <-reverted
 				moveToDistance(-55,MED_SPEED); //aligns with north PVC
 				moveToDistance(4, FAST_SPEED); //moves slightly forward to leave room for pivot
@@ -58,17 +55,18 @@ int main() {
 				gateOpen(); //drops all biofuels
 				
 				//LEG 5: navigate and drop blockers
-				
 				gateClose();
 				moveToDistance(-20,FAST_SPEED); //back up
 				pivot(0, 90, FAST_SPEED); //pivot to face east PVC
 				moveToDistance(30,FAST_SPEED); //Wall align against east PVC
 				moveToDistance(-55,FAST_SPEED); //go back to wall align to middle pvc
 				pivot(1,90, FAST_SPEED); //turn to face front of bot to north PVC
-				moveToDistance(20,FAST_SPEED); //forward wall align to north PVC
+	/**/		moveToDistance(6, FAST_SPEED); //forward wall align to north PVC
+				gateOpen();
+				//gateClose();
 				moveToDistance(-85,FAST_SPEED); //go to middle of board
 				pivot(1,-98,FAST_SPEED); //turn to face back to middle bump
-				gateOpen(); //incase the other dump didn't empty the balls
+				//gateOpen(); //incase the other dump didn't empty the balls
 				moveToDistance(-100,SUPER_SPEED); //go backward over bump
 					
 				//LEG 6: screwing with the opponent bots
