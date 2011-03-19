bcdb7e24a44617c70bac3e84ed60ce2743f10db7
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-19 08:43:26 +0000

    Committing aRobotDouble done for the night.

--- trunk/orangeelephants/2011/createbot/aRobotDouble.c
+++ trunk/orangeelephants/2011/createbot/aRobotDouble.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+	int LIGHT_PORT = 0;
 	printf("Now press the A button to run side A. \n");
 	printf("Now press the B button to run side B. \n");
 	createInitialize();
@@ -11,71 +12,76 @@ int main()
 	{
 		while(1)	{
 			if(a_button())
-			{	
-				//wait_for_light(LIGHT_PORT);
+			{					
+				wait_for_light(LIGHT_PORT);
 				float initSeconds = seconds();
 				printf("Init seconds = %d.\n", initSeconds);
-				//initialization
 				shut_down_in(120);
 				enable_servos();
-				
-				//Leg 1, starts and drops off airplane
 				moveToDistAccel(-5, NORMAL_SPEED);//first movement, backs up to travel down lane
 				smoothTurn(-90, 200);//Faces North
 				accel(0, 500);
-				moveToDist(400, 500);//Travels North the length of the board
+				moveToDist(200, 500);//Travels North to middle of board
 				accel(500, 0);
 				smoothTurn(-90, 200);//Turns towards runway, robot faces West
-				//Going in for airplane dropoff
 				while(1)	{
-					if(seconds() - initSeconds > 15) 
+					if(seconds() - initSeconds > 15) //wait 15 seconds before moving
 						break;					
 				}
 				printf("The time is %f.\n", seconds()-initSeconds);
-				moveToDist(750, 500);//drive forward to pick up block25
-				sleep(1);
-				moveToDist(500, 500);
-				beep();
-				sleep(.5);
-				beep();
-				sleep(.5);
-				beep();
+				moveToDist(1200, 500);//drive across the divider in the middle
+				smoothTurn(90, 200);//turn to face the north
+				moveToDist(200, 500);
 			}
+
 			if(b_button())
 			{	
-				//initialization
-				//wait_for_light(LIGHT_PORT);
-				int initSeconds = seconds();
+				wait_for_light(LIGHT_PORT);
+				float initSeconds = seconds();
+				printf("Init seconds = %d.\n", initSeconds);
 				shut_down_in(120);
-				
-				
-				//Leg 1, starts and drops off airplane
+				enable_servos();
 				moveToDistAccel(-5, NORMAL_SPEED);//first movement, backs up to travel down lane
-				smoothTurn(110, 200);//Faces North
+				smoothTurn(90, 200);//Faces North
 				accel(0, 500);
-				moveToDist(1100, 500);//Travels North the length of the board
-				accel(500, 50);
-				moveToDist(-7, 200);//Backs up from wall to allow turn
-				smoothTurn(-90, 200);//Turns towards runway, robot faces East
-				//Going in for airplane dropoff
-				moveStraight(-250);//Runs into East PVC
-				sleep(2.75);
-				create_stop();
-				releasePlane();
-				//Completed the airplane dropoff
-				
-				//Leg 2, picks up first set of blocks
-				moveToDist(225+25, 500);//backs up before turning to head back 
-				smoothTurn(94, 200);//Turns to face North
-				moveStraight(100);//Wall alignment w/ North PVC
-				sleep(.6);
-				accel(0, -1400+450);//drives back to align with blocks
-				accel(-1400+450, 0);
-				smoothTurn(94, 200);//turns East to face blocks
-				while(1)	{if(seconds() - initSeconds > 15) break;}
-				printf("The time is %d.\n", seconds()-initSeconds);
-				moveToDist(500, 500);//drive forward to pick up blocks
+				moveToDist(200, 500);//Travels North to middle of board
+				accel(500, 0);
+				smoothTurn(90, 200);//Turns towards runway, robot faces West
+				while(1)	{
+					if(seconds() - initSeconds > 15) //wait 15 seconds before moving
+						break;					
+				}
+				printf("The time is %f.\n", seconds()-initSeconds);
+				moveToDist(1200, 500);//drive across the divider in the middle
+				smoothTurn(-90, 200);//turn to face the north
+				moveToDist(200, 500);
 			}
+			/*
+			if(black_button())
+			{
+				printf("Press up to open, down to close, black button to finish -- move it to the perpendicular position");
+				while (1) 
+				{
+					if (up_button() == 1) {
+						mrp(DROPPER_PORT,200,-20);
+					}
+					if(down_button() == 1) {
+						mrp(DROPPER_PORT,200,20);
+					}
+					if(black_button() == 1) {
+						break;
+					}
+				}
+			}
+			
+			if(up_button())
+			{
+				mtp(DROPPER_PORT, 100, -40);
+				bmd(DROPPER_PORT);
+			}
+				
+			
+			*/
 		}//keep, ends while loop
  
 	}
