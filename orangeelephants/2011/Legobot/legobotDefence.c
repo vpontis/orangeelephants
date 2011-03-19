dfd66b84888c16545c52c8a5d1dcc5db7fc0544a
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-19 08:42:38 +0000

    Legobot Defence done for the night.

--- orangeelephants/2011/Legobot/legobotDefence.c
+++ orangeelephants/2011/Legobot/legobotDefence.c
@@ -34,13 +34,14 @@ int main() {
 				shut_down_in(117);
 				printf("\n Starting side A");
 				//LEG 1: collects ping pong balls @ first station
+				sleep(14);
 				moveToDist(49, MED_SPEED);
 				sleep(0.2);
 				moveToDist(3, MED_SPEED);
 				sleep(.7);
 				
 				//LEG 2: navigates to north PVC
-				moveToDist(-5, FAST_SPEED); //moves back to avoid tampering the red towers	
+				moveToDist(-6, FAST_SPEED); //moves back to avoid tampering the red towers	
 				sleep(10.5);
 				pivot(0,-90,FAST_SPEED); //pivots -90 degrees CW
 				moveToDist(-7-10,FAST_SPEED);//moves back to avoid pivoting on black tape
@@ -73,19 +74,11 @@ int main() {
 				pivot(0, -80, FAST_SPEED); //pivots to face drop zone
 				moveToDist(50, FAST_SPEED); //drives to and aligns with drop zone (north PVC)
 				gateOpen(); //drops all biofuels
-				
-				//LEG 5: navigate
-				gateClose();
-				moveToDist(-70,FAST_SPEED); //go to middle of board
-				pivot(1,-93,FAST_SPEED); //turn to face back to middle bump
-				//gateOpen(); //incase the other dump didn't empty the balls
-				moveToDist(-110,SUPER_SPEED); //go backward over bump
-					
-				//LEG 6: screwing with the opponent bots
-				moveToDist(-70,FAST_SPEED);
-				while(1){
-					moveToDist(60,FAST_SPEED);
-					moveToDist(-70,FAST_SPEED);
+				int i;
+				for (i = 0; i < 7; i++)	{
+					moveToDist(-0.5,FAST_SPEED);
+					sleep(0.1);
+					moveToDist(2,SUPER_SPEED);
 				}
 			}
 
@@ -95,6 +88,7 @@ int main() {
 				shut_down_in(117);
 				printf("\n Starting side B");
 				//LEG 1: collects ping pong balls @ first station
+				sleep(14.5);
 				moveToDist(50.5, MED_SPEED);
 				sleep(.2);
 				moveToDist(3, MED_SPEED);
@@ -104,16 +98,11 @@ int main() {
 				moveToDist(-8, FAST_SPEED); //moves back to avoid tampering the red towers	
 				sleep(2.5);
 				pivot(1,-90,FAST_SPEED); //pivots -90 degrees CCW
-				moveToDist(-5,FAST_SPEED);//moves back to avoid pivoting on black tape
+				moveToDist(-6-10,FAST_SPEED);//moves back to avoid pivoting on black tape
 				pivot(0,-91,FAST_SPEED); //pivots -90 degrees CW
 				
-				moveToDist(82, FAST_SPEED);//head to North PVC, the two distances should add up to 120
-				pivot(0,-70,FAST_SPEED);//pivot and allow Create to pass
-				moveToDist(-13,FAST_SPEED);
-				sleep(8.5);//wait for Create to pass
-				moveToDist(13,FAST_SPEED);
-				pivot(0,85, FAST_SPEED);
-				moveToDist(72, FAST_SPEED);
+				moveToDist(82+72, FAST_SPEED);//head to North PVC, the two distances should add up to 120
+				//moveToDist(72, FAST_SPEED);
 				
 //				moveToDist(80, FAST_SPEED); //starts distance to hit north PVC
 //				sleep(5); //to wait for the create to go past
@@ -123,7 +112,7 @@ int main() {
 				
 				moveToDist(-20,FAST_SPEED); //backs up to allow for turn
 				turn(90,SLOW_SPEED); //pivots to face east PVC
-				moveToDist(60,FAST_SPEED); //aligns with east PVC
+				moveToDist(60+10,FAST_SPEED); //aligns with east PVC
 				moveToDist(-6,FAST_SPEED); //backs up to allow for turn
 				turn(89,SLOW_SPEED); //pivots to face south PVC & ping pong balls
 				moveToDist(21, FAST_SPEED); //collects all biofuels
@@ -140,25 +129,11 @@ int main() {
 				moveToDist(50, FAST_SPEED); //drives to and aligns with drop zone (north PVC)
 				gateOpen(); //drops all biofuels
 				int i;
-				for (i = 0; i < 7; i++)
-				{
-				moveToDist(-0.5,FAST_SPEED);
-				sleep(0.1);
-				moveToDist(2,SUPER_SPEED);
+				for (i = 0; i < 7; i++) {
+					moveToDist(-0.5,FAST_SPEED);
+					sleep(0.1);
+					moveToDist(2,SUPER_SPEED);
 				}
-				//LEG 5: navigate to other side				
-				gateClose();
-				moveToDist(-75,FAST_SPEED); //go to middle of board
-				pivot(0,-98,FAST_SPEED); //turn to face back to middle bump
-				//gateOpen(); //incase the other dump didn't empty the balls
-				moveToDist(-110,SUPER_SPEED); //go backward over bump
-					
-				//LEG 6: screwing with the opponent bots
-				moveToDist(-70,FAST_SPEED);
-				while(1){
-					moveToDist(60,FAST_SPEED);
-					moveToDist(-70,FAST_SPEED);
-				}	
-			}	
+			}
 	}		
 }
