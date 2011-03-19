41a01731c8705c92acf6e212552f0cb0a9818c5f
VPontis@gmail.com <VPontis@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-03-19 20:13:45 +0000

    Create code committed.

--- trunk/orangeelephants/2011/createbot/aRobotDouble.c
+++ trunk/orangeelephants/2011/createbot/aRobotDouble.c
@@ -13,7 +13,7 @@ int main()
 		while(1)	{
 			if(a_button())
 			{					
-				wait_for_light(LIGHT_PORT);
+				//wait_for_light(LIGHT_PORT);
 				float initSeconds = seconds();
 				printf("Init seconds = %d.\n", initSeconds);
 				shut_down_in(120);
@@ -21,7 +21,7 @@ int main()
 				moveToDistAccel(-5, NORMAL_SPEED);//first movement, backs up to travel down lane
 				smoothTurn(-90, 200);//Faces North
 				accel(0, 500);
-				moveToDist(200, 500);//Travels North to middle of board
+				moveToDist(250, 500);//Travels North to middle of board
 				accel(500, 0);
 				smoothTurn(-90, 200);//Turns towards runway, robot faces West
 				while(1)	{
@@ -34,7 +34,7 @@ int main()
 
 			if(b_button())
 			{	
-				wait_for_light(LIGHT_PORT);
+				//wait_for_light(LIGHT_PORT);
 				float initSeconds = seconds();
 				printf("Init seconds = %d.\n", initSeconds);
 				shut_down_in(120);
@@ -42,7 +42,7 @@ int main()
 				moveToDistAccel(-5, NORMAL_SPEED);//first movement, backs up to travel down lane
 				smoothTurn(90, 200);//Faces North
 				accel(0, 500);
-				moveToDist(200, 500);//Travels North to middle of board
+				moveToDist(250, 500);//Travels North to middle of board
 				accel(500, 0);
 				smoothTurn(90, 200);//Turns towards runway, robot faces West
 				while(1)	{
@@ -52,32 +52,7 @@ int main()
 				printf("The time is %f.\n", seconds()-initSeconds);
 				moveToDist(1300, 500);//drive across the divider in the middle
 			}
-			/*
-			if(black_button())
-			{
-				printf("Press up to open, down to close, black button to finish -- move it to the perpendicular position");
-				while (1) 
-				{
-					if (up_button() == 1) {
-						mrp(DROPPER_PORT,200,-20);
-					}
-					if(down_button() == 1) {
-						mrp(DROPPER_PORT,200,20);
-					}
-					if(black_button() == 1) {
-						break;
-					}
-				}
-			}
 			
-			if(up_button())
-			{
-				mtp(DROPPER_PORT, 100, -40);
-				bmd(DROPPER_PORT);
-			}
-				
-			
-			*/
 		}//keep, ends while loop
  
 	}
