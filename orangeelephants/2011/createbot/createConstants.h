26fb7e6c29387cf64b71a99d1e1f0c0e705a7e73
prithvi.undavalli@gmail.com <prithvi.undavalli@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-06-23 22:34:15 +0000

    Added slider code.

--- orangeelephants/2011/createbot/createConstants.h
+++ orangeelephants/2011/createbot/createConstants.h
@@ -12,44 +12,16 @@ const int LIGHT_PORT_B = 0;
 const int  ARM_DOWN_VELOCITY = -400; //needs callibration
 const int  ARM_UP_VELOCITY = 750; //needs callibration
 
-//Up/down positions for the claw on the forklift 
-//Top is 0 and as you descend the position is negative
-const int  ARM_POS_DOWN = -8050;
-const int  ARM_POS_START = -6375;//up down position for the claw to start
-const int  ARM_PARTIAL_DOWN  = -600;
-
-//Opening/closing positions of the claw
-const int  CLAW_OPEN_START = 1000;
-const int  CLAW_OPEN_POS = 800;
-const int  CLAW_OPEN_PARTIAL = 350;
-const int  CLAW_CLOSE_POS = 0;
-
 //Motor, Sensor, & Servo ports
-const int  ARM_MOTOR_PORT = 0; 
-const int  CLAW_PORT = 2; 
-const int  ARM_TOUCH_PORT = 15;
-const int  GRABBER_PORT = 0;
-const int SCRAPERL_SERVO = 1; 
-const int SCRAPERR_SERVO = 3;
+const int  HOPPER = 0;
+const int  SLIDER_BACK = 3;
+const int  SLIDER_FRONT = 0;
 
 //speed
 const int FAST_SPEED = 1500;
 const int NORMAL_SPEED = 500;
 
-//positions at the start of game for scraper
-const int SCRAPERL_START = 0; 
-const int SCRAPERR_START = 1750; 
-
-//neutral positions for scraper during game
-const int SCRALERL_NEUTRAL = 920; 
-const int SCRAPERR_NEUTRAL = 850; 
-
-//positions for scraper when going for botguy
-const int SCRAPERL_DOWN = 1900;
-const int SCRAPERR_DOWN = 0;
-
-//positions for grabber
-const int GRABBER_UP = 1400;
-const int GRABBER_DOWN = 750;
+//hopper positions
+const int  HOPPER_CLOSE = 1850;
 
 #endif
--- orangeelephants/2011/createbot/createFunctions.h
+++ orangeelephants/2011/createbot/createFunctions.h
@@ -1,24 +1,7 @@
 #ifndef _CREATEFUNCTIONS_H_
 #define _CREATEFUNCTIONS_H_
 void createInitialize();
-
-void moveClawUp();
-void moveClawDown(int nDistance);
-void pickUpDropOff();
-void slowReleaseClaw();
-void slowCloseClaw();
-void pickUpBlocks();
-
-void moveArm(int position, int speed);
-void setClaw(int position);
-
-void openClaw();
-void closeClaw();
-void openClawPartial();
-
-void scraperStart();
-void scraperNeutral();
-void scraperDown();
+void moveSlider(int pos, int speed);
 
 
 #endif
