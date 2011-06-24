b22cb57a4b2430ba807fbddf005641ac630ef1cd
eLpWnZoR007@gmail.com <eLpWnZoR007@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-06-24 00:16:51 +0000

    claw and arm things

--- trunk/orangeelephants/2011 Nats/Createbot/createFunctions.h
+++ trunk/orangeelephants/2011 Nats/Createbot/createFunctions.h
@@ -1,7 +1,13 @@
 #ifndef _CREATEFUNCTIONS_H_
 #define _CREATEFUNCTIONS_H_
 void createInitialize();
+
 void moveSlider(int pos, int speed);
+
+void moveArm(int pos); 
+void calibrateArm(); 
+void closeClaw(); 
+void openClaw(); 
 
 
 #endif
