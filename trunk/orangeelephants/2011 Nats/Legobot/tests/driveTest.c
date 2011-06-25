86b09b7a7a7292dede91fc0b49d29cca9b247623
joshuasend1@gmail.com <joshuasend1@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-06-25 23:44:56 +0000

    worked on legobot main and added a folder for tests

---
+++ trunk/orangeelephants/2011 Nats/Legobot/tests/driveTest.c
@@ -0,0 +1,12 @@
+#include "legobotFunctions.c"
+#include "legobotConstants.h"
+
+int main() {
+	int x = 1;
+	while (x <= 10) {
+		printf("%d",x*100);
+		moveToDist(50, x*100);
+		moveToDist(-50,x*100);
+		x++;
+	}
+}
---
+++ trunk/orangeelephants/2011 Nats/Legobot/tests/legobotClaw.c
@@ -0,0 +1,20 @@
+#include "legobotFunctions.c"
+
+int main()
+{
+	clear_motor_position_counter(R_MOTOR); 
+	clear_motor_position_counter(L_MOTOR);
+	set_servo_position(ARM_PORT, 800); 
+	set_servo_position(CLAW_PORT, 1600); 
+	
+	printf("A Button to start"); 
+	while (1) {
+		if (a_buttton()) {
+			raiseArm(); 
+			moveToDist(60, 500); 
+			lowerArm(); 
+			closeClaw(); 
+			raiseArm(); 
+		}
+	}
+}
