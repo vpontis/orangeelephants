2792b3c2381d20ac8c2598671e79881fdfdb1d89
prithvi.undavalli@gmail.com <prithvi.undavalli@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-06-26 00:26:31 +0000

    Minor edits to documentation.

--- trunk/orangeelephants/2011/createbot/createFunctions.c
+++ trunk/orangeelephants/2011/createbot/createFunctions.c
@@ -1,6 +1,10 @@
 #include "createFunctions.h"
 #include "createConstants.h"
+#include "createVision.c"
+#include "createMotor.c"
 
+/** \brief Connects create to CBC and sets to full mode.
+*/
 void createInitialize()
 {
 	int conVal = create_connect();
@@ -10,15 +14,19 @@ void createInitialize()
 	else{
 		printf("Connection failed.");
 	}
+	
+	//connected successfully! Place in full mode. 
 	create_full();
 }
 
-//Moves the claw to pinch the blocks up until it hits the sensor
+/** \brief Function to move horizontal slider system
+	\todo seperate into individual functions
+*/
 void moveSlider(int pos, int speed){
-	
 	clear_motor_position_counter(SLIDER_BACK);
 	clear_motor_position_counter(SLIDER_FRONT);
-
+	
+	//TODO: Move commands into seperate functions
 	mrp(SLIDER_BACK, speed, pos);
 	mrp(SLIDER_FRONT, -speed, pos);
 	bmd(SLIDER_BACK);
@@ -28,6 +36,11 @@ void moveSlider(int pos, int speed){
 	clear_motor_position_counter(SLIDER_FRONT);
 }
 
+/** \brief Closes hopper
+*/
 void closeHopper(){
 	set_servo_position(HOPPER, HOPPER_CLOSE);
 }
+
+
+
