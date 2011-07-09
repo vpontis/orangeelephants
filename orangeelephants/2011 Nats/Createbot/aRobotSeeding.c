a7ab3e36bf5e6ed69458d767ca50aab370c91fb1
elpwnzor007@gmail.com <elpwnzor007@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-07-09 18:31:49 +0000

    lowered support a little bit. changed some values for seeding

--- orangeelephants/2011 Nats/Createbot/aRobotSeeding.c
+++ orangeelephants/2011 Nats/Createbot/aRobotSeeding.c
@@ -8,12 +8,14 @@ void sideB() {
 	closeClaw(); 
 
 	//move towards first set of blocks
-	createMove(NORMAL_SPEED, 1.569); 
+	createMove(NORMAL_SPEED, 1.49); //
 	turn(-90); 
 	openClaw();		 
-	createMove(NORMAL_SPEED, 1.00); 	 
+	createMove(NORMAL_SPEED, 1.00); 	
+	//createCenter(0,6,MIN_CONFIDENCE_SHORT); 
 		
 	//pick up first set of blocks
+	//createMove(SLOW_SPEED, 1.83);
 	touchSensorMovement(); 
 	closeClaw(); 
 	sleep(.5); 
@@ -22,18 +24,19 @@ void sideB() {
 	//move towards second set of blocks
 	createMove(-NORMAL_SPEED, .6); 
 	turn(90); 
-	createMove(NORMAL_SPEED, 1.895); 
+	createMove(NORMAL_SPEED, 1.94); 
 	turn(-90); 
 		
 	//drop first set of blocks on top of second
 	createMove(NORMAL_SPEED, .1); 
 	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
 	createMove(SLOW_SPEED, 2.45);  	
-	createMove(-SLOW_SPEED,.63); 
+	createMove(-SLOW_SPEED,.69); 
+	moveArm(ARM_POS_MID); 
 	sleep(.5); 
 	openClawIncrement(); 
 	sleep(.5);
-	
+			
 	//move to other side of board
 	moveArm(ARM_POS_UP); 
 	createMove(-FAST_SPEED, .25); 
@@ -42,9 +45,9 @@ void sideB() {
 	createMove(NORMAL_SPEED, .8); 
 	turn(90); 
 	moveArm(ARM_POS_MID); 
-	while ((seconds()-startTime)<29.6) {}
+	while((seconds()-startTime)<29.6) {}
 	createMove(FAST_SPEED, 2.37); 
-	
+		
 	//pick up third set of blocks
 	turn(-90); 
 	createMove(-NORMAL_SPEED,.77); 
@@ -64,30 +67,32 @@ void sideB() {
 	turn(-90); 
 	createMove(-FAST_SPEED, .6); 
 	turn(90);
-	createMove(FAST_SPEED, 3.00);
-	
+	createMove(FAST_SPEED, 2.68);
+		
 	//Drop off blocks near starting box.
 	moveArm(ARM_POS_DOWN); 
-	turn(84);
+	turn(81);
 	createMove(FAST_SPEED, .92);
 	bmd(ARM_MOTOR_PORT); 
 	openClaw(); 
 	moveArm(ARM_POS_UP); 
-	
+			
 	//Move to stack of four
+	createMove(-FAST_SPEED, .5); 
+	turn(9); 
 	int bumperStart = seconds(); 
 	while( get_create_rbump(.01) != 1) { 
 		moveStraight(-FAST_SPEED);
-		if ((seconds() - bumperStart) > 5) {
+		if ((seconds() - bumperStart) > 8) {
 			break; 
-			}
+		}
 	}
 	createStop(); 
 	createMove(NORMAL_SPEED, .69); 
 	turn(90); 
 	createMove(NORMAL_SPEED, .3); 
 	moveArm(ARM_POS_MID); 
-		
+			
 	//pick up stack of four
 	moveSupport(SUPPORT_POS_UP); 
 	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
@@ -97,13 +102,15 @@ void sideB() {
 	touchSensorMovement(); 
 	closeClawIncrement(); 
 	sleep(.5); 
+	
 	if ((seconds()-startTime)>130) {
 		moveArm(ARM_POS_UP); 
 		createMove(-SLOW_SPEED, 2); 
-		smoothTurn(130,150); 
+		create_drive(50, 1); 
+		sleep(4.4); 
 		createMove(SLOW_SPEED, 3.5); 
-		accel(0,300); 
-		accel(300,0); 
+		accel(0,245); 
+		accel(245,0); 
 		createStop(); 
 		moveArm(ARM_POS_DOWN); 
 		sleep(.5); 
@@ -111,20 +118,21 @@ void sideB() {
 		sleep(.5); 
 		createMove(-FAST_SPEED, .8); 
 	}
+	
 	//Drop off stack of four on stack of two
 	moveArm(ARM_POS_UP); 
 	createMove(-SLOW_SPEED, 2); 
 	create_drive(50, 1); 
 	sleep(4.4); 
 	create_stop();    
-	createMove(SLOW_SPEED, 3.3);  
+	createMove(SLOW_SPEED, 3.5);  
 	createStop(); 
 	createCenter(0,15,MIN_CONFIDENCE_LONG); 
-	accel(0,245);  
-	accel(245,0); 
+	accel(0,220);  
+	accel(220,0); 
 	createStop(); 
 	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
-	createMove(SLOW_SPEED, 2.4);  
+	createMove(SLOW_SPEED, 2.0);  
 	createMove(-SLOW_SPEED, .6); 
 	if (CANT_SEE==false) {
 		moveArm(ARM_POS_MID); 
@@ -169,7 +177,7 @@ void sideA() {
 	closeClaw(); 
 
 	//move towards first set of blocks
-	createMove(NORMAL_SPEED, 1.569); //
+	createMove(NORMAL_SPEED, 1.49); //
 	turn(90); 
 	openClaw();		 
 	createMove(NORMAL_SPEED, 1.00); 	
@@ -185,14 +193,16 @@ void sideA() {
 	//move towards second set of blocks
 	createMove(-NORMAL_SPEED, .6); 
 	turn(-90); 
-	createMove(NORMAL_SPEED, 1.905); 
+	createMove(NORMAL_SPEED, 1.94); 
 	turn(90); 
 		
 	//drop first set of blocks on top of second
 	createMove(NORMAL_SPEED, .1); 
 	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
 	createMove(SLOW_SPEED, 2.45);  	
-	createMove(-SLOW_SPEED,.63); 
+	createMove(-SLOW_SPEED,.69); 
+	moveArm(ARM_POS_MID); 
+	bmd(ARM_MOTOR_PORT); 
 	sleep(.5); 
 	openClawIncrement(); 
 	sleep(.5);
@@ -227,21 +237,23 @@ void sideA() {
 	turn(90); 
 	createMove(-FAST_SPEED, .6); 
 	turn(-90);
-	createMove(FAST_SPEED, 3.00);
+	createMove(FAST_SPEED, 2.68);
 		
 	//Drop off blocks near starting box.
 	moveArm(ARM_POS_DOWN); 
-	turn(-84);
+	turn(-81);
 	createMove(FAST_SPEED, .92);
 	bmd(ARM_MOTOR_PORT); 
 	openClaw(); 
 	moveArm(ARM_POS_UP); 
 			
 	//Move to stack of four
+	createMove(-FAST_SPEED, .5); 
+	turn(-9); 
 	int bumperStart = seconds(); 
 	while( get_create_rbump(.01) != 1) { 
 		moveStraight(-FAST_SPEED);
-		if ((seconds() - bumperStart) > 5) {
+		if ((seconds() - bumperStart) > 8) {
 			break; 
 		}
 	}
@@ -264,10 +276,11 @@ void sideA() {
 	if ((seconds()-startTime)>130) {
 		moveArm(ARM_POS_UP); 
 		createMove(-SLOW_SPEED, 2); 
-		smoothTurn(130,150); 
+		create_drive(-50, 1); 
+		sleep(4.4); 
 		createMove(SLOW_SPEED, 3.5); 
-		accel(0,300); 
-		accel(300,0); 
+		accel(0,245); 
+		accel(245,0); 
 		createStop(); 
 		moveArm(ARM_POS_DOWN); 
 		sleep(.5); 
@@ -285,11 +298,11 @@ void sideA() {
 	createMove(SLOW_SPEED, 3.5);  
 	createStop(); 
 	createCenter(0,15,MIN_CONFIDENCE_LONG); 
-	accel(0,245);  
-	accel(245,0); 
+	accel(0,220);  
+	accel(220,0); 
 	createStop(); 
 	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
-	createMove(SLOW_SPEED, 2.4);  
+	createMove(SLOW_SPEED, 2.0);  
 	createMove(-SLOW_SPEED, .6); 
 	if (CANT_SEE==false) {
 		moveArm(ARM_POS_MID); 
