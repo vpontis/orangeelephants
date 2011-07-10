05dee07b42ee14b8ba335625eaecdf5d352dc065
elpwnzor007@gmail.com <elpwnzor007@gmail.com@971c5f28-b4e8-81da-5658-e984a7b5cbdc>
2011-07-10 22:15:57 +0000

    all values up to moving onto opposing side are updated to latest.

--- trunk/orangeelephants/2011 Nats/Createbot/aRobotDouble.c
+++ trunk/orangeelephants/2011 Nats/Createbot/aRobotDouble.c
@@ -1,113 +1,134 @@
 #include "createFunctions.c"
 
-int main()
-{  
+void sideA();
+void sideB();
+
+void sideB() {
+	int startTime = seconds(); 
+	closeClaw(); 
+
+	//move towards first set of blocks
+	createMove(NORMAL_SPEED, 1.49); 
+	turn(-90); 
+	openClaw();		 
+	createMove(NORMAL_SPEED, 1.00); 	
+		
+	//pick up first set of blocks
+	touchSensorMovement(); 
+	closeClaw(); 
+	sleep(.5); 
+	moveArm(ARM_POS_UP);  
+		
+	//move towards second set of blocks
+	createMove(-NORMAL_SPEED, .6); 
+	turn(90); 
+	createMove(NORMAL_SPEED, 1.95); 
+	turn(-90); 
+		
+	//drop first set of blocks on top of second
+	createMove(NORMAL_SPEED, .1); 
+	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
+	createMove(SLOW_SPEED, 2.45);  	
+	createMove(-SLOW_SPEED,.69); 
+	moveArm(ARM_POS_MID); 
+	sleep(.5); 
+	openClawIncrement(); 
+	sleep(.5);
+			
+	//move to other side of board
+	moveArm(ARM_POS_UP); 
+	createMove(-FAST_SPEED, .25); 
+	//adjustTurn(); 
+	turn(-90); 
+	createMove(NORMAL_SPEED, .8); 
+	turn(90); 
+	moveArm(ARM_POS_MID); 
+	while((seconds()-startTime)<29.6) {}
+	createMove(FAST_SPEED, 3); 
+	while(1) {
+		createMove(-FAST_SPEED, 1.5); 
+		createMove(FAST_SPEED, 1.5); 
+	}
+}
+
+/*******************************************************/
+
+void sideA() {
+	int startTime = seconds(); 
+	closeClaw(); 
+
+	//move towards first set of blocks
+	createMove(NORMAL_SPEED, 1.49); 
+	turn(90); 
+	openClaw();		 
+	createMove(NORMAL_SPEED, 1.00); 	
+		
+	//pick up first set of blocks
+	touchSensorMovement(); 
+	closeClaw(); 
+	sleep(.5); 
+	moveArm(ARM_POS_UP);  
+		
+	//move towards second set of blocks
+	createMove(-NORMAL_SPEED, .6); 
+	turn(-90); 
+	createMove(NORMAL_SPEED, 1.92); 
+	turn(90); 
+		
+	//drop first set of blocks on top of second
+	createMove(NORMAL_SPEED, .1); 
+	createCenter(0,6,MIN_CONFIDENCE_SHORT); 
+	createMove(SLOW_SPEED, 2.45);  	
+	createMove(-SLOW_SPEED,.69); 
+	moveArm(ARM_POS_MID); 
+	sleep(.5); 
+	openClawIncrement(); 
+	sleep(.5);
+			
+	//move to other side of board
+	moveArm(ARM_POS_UP); 
+	createMove(-FAST_SPEED, .25); 
+	//adjustTurn(); 
+	turn(90); 
+	createMove(NORMAL_SPEED, .8); 
+	turn(-90); 
+	moveArm(ARM_POS_MID); 
+	while((seconds()-startTime)<29.6) {}
+	createMove(FAST_SPEED, 3); 
+	while(1) {
+		createMove(-FAST_SPEED, 1.5); 
+		createMove(FAST_SPEED, 1.5); 
+	}
+}
+
+int main() {
 	enable_servos();
 	createInitialize();
 	calibrateArm();
 	openClaw(); 
 	moveArm(ARM_POS_DOWN);
-	printf("Main Menu \n Press B to execute code. Press L/R or A for testing.");
-	while (1) {
-			if (a_button()) {
-			shut_down_in(117.5); 
-			int startTime = seconds(); 
-			closeClaw(); 
-				
-			//move towards first set of blocks
-			createMove(NORMAL_SPEED, 1.569); //
-			turn(-90); 
-			openClaw();		 
-			createMove(NORMAL_SPEED, 1.00); 	
-			
-			//pick up first set of blocks
-			createMove(SLOW_SPEED, 1.8); 
-			closeClaw(); 
-			sleep(.5); 
-			moveArm(ARM_POS_UP);  
-			
-			//move towards second set of blocks
-			createMove(-NORMAL_SPEED, .6); 
-			turn(90); 
-			createMove(NORMAL_SPEED, 1.915); 
-			turn(-90); 
-			
-			//drop first set of blocks on top of second
-			createMove(NORMAL_SPEED, .1); 
-			//createCenter(0,6,MIN_CONFIDENCE_SHORT); 
-			createMove(SLOW_SPEED, 2.3);  	
-			createMove(-SLOW_SPEED,.49); 
-			sleep(.5); 
-			openClaw(); 
-			sleep(.5);
-			
-			//move to other side of board
-			moveArm(ARM_POS_UP); 
-			createMove(-FAST_SPEED, .25); 
-			//adjustTurn(); 
-			turn(-90); 
-			createMove(NORMAL_SPEED, .7); 
-			turn(90); 
-			while((seconds()-startTime)<30) {}
-			createMove(FAST_SPEED, 3); 
-			while (1) {
-				createMove(-FAST_SPEED, 1.5); 
-				createMove(FAST_SPEED, 1.5); 
-			}
-		}
-		if (left_button()) {
-			printf("Turning Compensation Ratio: %f \n", TURN_COMP_RATIO); 
-			turn(-90); 
-		}
-		if (right_button()) {
-			printf("Turning Compensation Ratio: %f \n", TURN_COMP_RATIO); 
-			turn(90); 
-		}
-		if (b_button()) {
-			shut_down_in(117.5); 
-			int startTime = seconds(); 
-			closeClaw(); 
+	moveSupport(SUPPORT_POS_DOWN); 
 	
-			//move towards first set of blocks
-			createMove(NORMAL_SPEED, 1.569); //
-			turn(90); 
-			openClaw();		 
-			createMove(NORMAL_SPEED, 1.00); 	
-			
-			//pick up first set of blocks
-			createMove(SLOW_SPEED, 1.8); 
-			closeClaw(); 
-			sleep(.5); 
-			moveArm(ARM_POS_UP);  
-			
-			//move towards second set of blocks
-			createMove(-NORMAL_SPEED, .6); 
-			turn(-90); 
-			createMove(NORMAL_SPEED, 1.915); 
-			turn(90); 
-			
-			//drop first set of blocks on top of second
-			createMove(NORMAL_SPEED, .1); 
-			//createCenter(0,6,MIN_CONFIDENCE_SHORT); 
-			createMove(SLOW_SPEED, 2.3);  	
-			createMove(-SLOW_SPEED,.49); 
-			sleep(.5); 
-			openClaw(); 
-			sleep(.5);
-			
-			//move to other side of board
-			moveArm(ARM_POS_UP); 
-			createMove(-FAST_SPEED, .25); 
-			//adjustTurn(); 
-			turn(90); 
-			createMove(NORMAL_SPEED, .7); 
-			turn(-90); 
-			while((seconds()-startTime)<30) {}
-			createMove(FAST_SPEED, 3); 
-			while (1) {
-				createMove(-FAST_SPEED, 1.5); 
-				createMove(FAST_SPEED, 1.5); 
-			}
+	printf("Main Menu \n Press A for side A and B for side B. Press L/R for testing.");
+	
+	while (1) {
+		
+		if (a_button()) {
+			wait_for_light(LIGHT_PORT); 
+			run_for(110,sideA); 
+			create_stop(); 
+			disable_servos(); 
+			beep(); 
+			ao(); 
+		}
+
+		if (b_button()) {
+			wait_for_light(LIGHT_PORT); 
+			run_for(110, sideB); 
+			create_stop(); 
+			disable_servos(); 
+			beep(); 
+			ao(); 
 		}
 	}
 }
